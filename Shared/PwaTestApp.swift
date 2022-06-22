9b9210e04e2b645dd10ff29e4a5d7d22d8db68b5
Chetan Raina <chetan5896@gmail.com>
2022-06-22 22:56:37 +0530

    Initial Commit

---
+++ Shared/PwaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PwaTestApp.swift
+//  Shared
+//
+//  Created by Chetan Raina on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PwaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
