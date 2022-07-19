6e4dbe2b27687e7ffcb70d8df22542e254857c4e
Chetan Raina <chetan5896@gmail.com>
2022-07-19 16:54:04 +0530

    undo test changes

--- Shared/PwaWebView.swift
+++ Shared/PwaWebView.swift
@@ -12,28 +12,34 @@ import WebKit
 struct PwaWebView: UIViewRepresentable {
     
     class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
+        var webView: WKWebView?
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            self.webView = webView
+        }
         
         // receive message from wkwebview
         func userContentController(
             _ userContentController: WKUserContentController,
             didReceive message: WKScriptMessage
         ) {
-            
+            print(message.body)
+            let date = Date()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.messageToWebview(msg: "hello, I got your messsage: \(message.body) at \(date)")
+            }
         }
         
+        func messageToWebview(msg: String) {
+            self.webView?.evaluateJavaScript("webkit.messageHandlers.bridge.onMessage('\(msg)')")
+        }
     }
     
     func makeCoordinator() -> Coordinator {
         return Coordinator()
     }
     
-    func makeUIView(context: Context) -> some UIView {
-//        let webView = WKWebView()
-//
-//        let request = URLRequest(url: url)
-//        webView.load(request)
-//        return webView
-        
+    func makeUIView(context: Context) -> WKWebView {
         let coordinator = makeCoordinator()
         let userContentController = WKUserContentController()
         userContentController.add(coordinator, name: "bridge")
@@ -41,10 +47,13 @@ struct PwaWebView: UIViewRepresentable {
         let configuration = WKWebViewConfiguration()
         configuration.userContentController = userContentController
         let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.navigationDelegate = coordinator
         
-        let url = URL(string: "http://localhost:3000/?sessionToken=6Jv9UhDufGfAjjgSUsz6Wr6UIVqB2bV0")!
+//        this works
+        let url = (Bundle.main.url(forResource: "index", withExtension: "html"))!
         
-//        let url = (Bundle.main.url(forResource: "index", withExtension: "html"))!
+//        this doesn't
+//         let url = URL(string: "http://localhost:3000/?sessionToken=5yq98jHJNp2V3XvTmg2xJUVWRf2WftNC")!
         
         let request = URLRequest(url: url)
         webView.load(request)
@@ -54,10 +63,4 @@ struct PwaWebView: UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
         
     }
-    
-//    var body: some View {
-//        NavigationView {
-//            Text("Welcome to the shop")
-//        }
-//    }
 }
